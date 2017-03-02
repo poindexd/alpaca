44ae6339b520eb013ba31f44d0e6c19bc30b87d9
Haemin Park <haeminp@umich.edu>
2017-03-02 14:14:44 -0500

    UI

--- dist/js/alpaca-editor.js
+++ dist/js/alpaca-editor.js
@@ -6956,28 +6956,46 @@ angular.module('alpacaEditor')
 
 		$scope.surveys = [
 			{
-				title: 'Inflammatory Bowel Disease Survey', author: 'Haemin Park', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2016/05/ibd-420x315.jpg', date: "01-20-2017"
+				title: 'Inflammatory Bowel Disease', organization: 'Haemin Park', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2016/05/ibd-420x315.jpg', date: "01-20-2017"
 			},
 			{
-				title: 'Pregnancy and Childbirth Survey', author: 'No one', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/pcb-420x315.jpg', date: "02-20-2017"
+				title: 'Pregnancy and Childbirth', organization: 'No one', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/pcb-420x315.jpg', date: "02-20-2017"
 			},
 			{
-				title: 'Overactive Bladder Survey', author: 'Poo', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/oab-1-420x315.jpg', date: "01-23-2017"
+				title: 'Overactive Bladder', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/oab-1-420x315.jpg', date: "01-23-2017"
 			},
 			{
-				title: 'Survey 4', author: 'Poo', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/crc-420x315.jpg', date: "01-01-2017"
+				title: 'Colorectal Cancer', organization: 'Wellopp', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/crc-420x315.jpg', date: "01-01-2017"
 			},
 			{
-				title: 'Survey 5', author: 'Poo', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/bc-420x315.jpg', date: "01-20-2017"
+				title: 'Breast Cancer', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/bc-420x315.jpg', date: "01-20-2017"
 			},
 			{
-				title: 'Survey 6', author: 'Poo', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/hf-420x315.jpg', date: "01-20-2017"
+				title: 'Heart Failure', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/hf-420x315.jpg', date: "01-20-2017"
 			},
 			{
-				title: 'Survey 7', author: 'Poo', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/condi_dementia-420x315.jpg', date: "01-20-2017"
+				title: 'Older Person', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/condi_dementia-420x315.jpg', date: "01-20-2017"
 			},
 			{
-				title: 'Survey 8', author: 'Poo', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/bp_level_of_pain_01_455x620-420x315.jpg', date: "01-20-2017"
+				title: 'Low Back Pain', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/bp_level_of_pain_01_455x620-420x315.jpg', date: "01-20-2017"
+			},
+			{
+				title: 'Stroke', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/05/resized-for-WordPress-840x630.jpg', date: "01-20-2017"
+			},
+			{
+				title: 'Lung Cancer', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/02/lc_01_455x620-420x315.jpg', date: "01-20-2017"
+			},
+			{
+				title: 'Depression and Anxiety', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/da_01_455x620-420x315.jpg', date: "01-20-2017"
+			},
+			{
+				title: 'Cleft Lip and Palate', organization: 'Wellopp', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2013/10/clp-420x315.jpg', date: "01-20-2017"
+			},
+			{
+				title: 'Parkinsons Desease', organization: '💩', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/parkinsons_disease-420x315.jpg', date: "01-20-2017"
+			},
+			{
+				title: 'Craniofacial Microsomia', organization: 'Wellopp', summary: "This survey is dumb", img_url: 'http://www.ichom.org/wp-content/uploads/2015/03/cfm-420x315.jpg', date: "01-20-2017"
 			}
 		];
 
