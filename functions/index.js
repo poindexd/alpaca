5b76b7df277c13b6bedd237cf14164ab82d77932
poindexd <danmanpoindexter@gmail.com>
2017-04-10 01:06:17 -0400

    scoring function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,39 @@
 var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+var jsonata = require('jsonata');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+admin.initializeApp(functions.config().firebase);
+
+exports.score = functions.database.ref('results')
+	.onWrite(event => {
+	
+	var survey = event.data.val();
+	console.log(survey);
+	
+	var endpointsRef = admin.database().ref('endpoints')
+		.orderByChild('collection_id')
+		.equalTo(survey.id);
+
+	var scoredRef = admin.database().ref('scored');
+
+	scoredRef.push({'test':true});
+
+	//Get endpoints for the collection
+	endpointsRef.once('value').then(snapshot => {
+		//For each endpoint, score and push result
+		snapshot.forEach(child => {
+			var endpoint = child.val();
+			var scored = jsonata(endpoint.expression).evaluate(survey);
+
+			//Push to hotpath DB
+			scoredRef.push(scored);
+
+			//Push to 3rd Party endpoint
+			//Requires Firebase paid plan
+			//...http.post(scored, endpoint.url)...
+
+		});
+	});
+	
+	res.end();
+});
