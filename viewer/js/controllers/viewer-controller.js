97f1ea247d700496c96b3c0ecd023b0c57cb9c8e
Haemin Park <haeminp@umich.edu>
2017-04-10 16:52:27 -0400

    more fixes

--- viewer/js/controllers/viewer-controller.js
+++ viewer/js/controllers/viewer-controller.js
@@ -2,36 +2,65 @@ angular.module('alpacaViewer').controller('alpacaViewerController', ['$scope', f
 	$scope.survey = {
 
 		slides: [
+			// {
+			// 	//id: 1, template: 'index'
+			// }
+			// ,
 			{
-				id: 1, title: 'Slide 1', template: 'green'
+				id: 2, template: 'likert_image_left', 
+				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.', 
+				content: "likert image left", 
+				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			},
 			{
-				id: 2, title: 'Slide 2', template: 'orange'
+				id:3, template: 'likert_image_none',
+				content: "likert image none Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nibh eros, placerat a vulputate quis, faucibus quis risus.",
+				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
+			
 			},
 			{
-				id: 3, question: 'Multiple choice with image', template: 'multiplechoice', options:[{text: "A"}, {text: "B"}, {text: "C"}, {text: "D"}, {text: "E"}] 
-				, image_align: "Left", image_url: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.', content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+				id:4, template: 'likert_image_right',
+				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.',
+				content: "likert image right",
+				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			},
 			{
-				id: 4, question: 'Multiple choice without image', template: 'multiplechoice_sans_image', options:[{text: "A"}, {text: "B"}, {text: "C"}, {text: "D"}, {text: "E"}] 
-				, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nibh eros, placerat a vulputate quis, faucibus quis risus."
+				id: 5, template: 'multiple_choice_image_left', 
+				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.', 
+				content: "multiplechoice image left", 
+				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			},
 			{
-				id: 5, question: 'Likert with image', template: 'likert', options:[{text: "strongly agree"}, {text: "agree"}, {text: "neutral"}, {text: "disagree"}, {text: "strongly disagree"}] 
-				, image_align: "Left", image_url: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.', content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+				id:6, template: 'multiple_choice_image_none',
+				content: "multiplechoice image none",
+				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
+			
 			},
 			{
-				id: 6, question: 'Likert without image', template: 'likert_sans_image', options:[{text: "strongly agree"}, {text: "agree"}, {text: "neutral"}, {text: "disagree"}, {text: "strongly disagree"}] 
-				, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nibh eros, placerat a vulputate quis, faucibus quis risus."
+				id:7, template: 'multiple_choice_image_right',
+				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.',
+				content: "multiplechoice image right",
+				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			},
 			{
-				id: 7, question: 'Testing Just Plain image', template: 'image', image_align: "Top", image_url: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.'
-				, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nibh eros, placerat a vulputate quis, faucibus quis risus."
+				id:8, template: 'text_image_left',
+				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.',
+				content: "text image left"
+			},
+			{
+				id:9, template: 'text_image_none',
+				content: "text image none"
+			},
+			{
+				id:10, template: 'text_image_right',
+				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.',
+				content: "text image right"
 			}
+			
 		]
 
 	}
 
-	$scope.selected = $scope.survey.slides[4];
+	$scope.selected = $scope.survey.slides[2];
 
 }]);
