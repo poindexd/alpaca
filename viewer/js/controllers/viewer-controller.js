a02be4572480fbe8c7bc7770854ffa920e42ebb3
poindexd <danmanpoindexter@gmail.com>
2017-04-10 23:45:08 -0400

    fix merge

--- viewer/js/controllers/viewer-controller.js
+++ viewer/js/controllers/viewer-controller.js
@@ -2,8 +2,6 @@ angular.module('alpacaViewer')
 .controller('alpacaViewerController', [
 	'$scope',
 	'$timeout',
-
-<<<<<<< HEAD
 	function($scope, $timeout){
 
 		$scope.survey = {
@@ -90,65 +88,6 @@ angular.module('alpacaViewer')
 			},
 			result: function(key, value, slide){
 				console.log(key, value, slide);
-=======
-		slides: [
-			// {
-			// 	//id: 1, template: 'index'
-			// }
-			// ,
-			{
-				id: 2, template: 'likert_image_left', 
-				image: '/img/image-placeholder_grey.svg', 
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ", 
-				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
-			},
-			{
-				id:3, template: 'likert_image_none',
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
-			
-			},
-			{
-				id:4, template: 'likert_image_right',
-				image: '/img/image-placeholder_grey.svg',
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
-			},
-			{
-				id: 5, template: 'multiple_choice_image_left', 
-				image: '/img/image-placeholder_grey.svg', 
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ", 
-				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
-			},
-			{
-				id:6, template: 'multiple_choice_image_none',
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
-			
-			},
-			{
-				id:7, template: 'multiple_choice_image_right',
-				image: '/img/image-placeholder_grey.svg',
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
-			},
-			{
-				id:8, template: 'text_image_left',
-				image: '/img/image-placeholder_grey.svg',
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. "
-			},
-			{
-				id:9, template: 'text_image_none',
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. "
-			},
-			{
-				id:10, template: 'text_image_right',
-				image: '/img/image-placeholder_grey.svg',
-				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. "
-			}
-			
-		]
->>>>>>> 43a20aeb5998bf4bdd814b82099eba00d9ff4bec
 
 				if(slide){
 					$scope.result[slide.id] = $scope.result[slide.id] || {};
