a013184203d74fb510c5859196c2c498f4a9b819
Haemin Park <haeminp@umich.edu>
2017-04-10 23:38:32 -0400

    hamster hehehehehe

--- dist/js/alpaca-viewer.js
+++ dist/js/alpaca-viewer.js
@@ -715,53 +715,53 @@ angular.module('alpacaViewer').controller('alpacaViewerController', ['$scope', f
 			// ,
 			{
 				id: 2, template: 'likert_image_left', 
-				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.', 
-				content: "likert image left", 
+				image: '/img/image-placeholder_grey.svg', 
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ", 
 				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			},
 			{
 				id:3, template: 'likert_image_none',
-				content: "likert image none Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nibh eros, placerat a vulputate quis, faucibus quis risus.",
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
 				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			
 			},
 			{
 				id:4, template: 'likert_image_right',
-				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.',
-				content: "likert image right",
+				image: '/img/image-placeholder_grey.svg',
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
 				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			},
 			{
 				id: 5, template: 'multiple_choice_image_left', 
-				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.', 
-				content: "multiplechoice image left", 
+				image: '/img/image-placeholder_grey.svg', 
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ", 
 				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			},
 			{
 				id:6, template: 'multiple_choice_image_none',
-				content: "multiplechoice image none",
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
 				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			
 			},
 			{
 				id:7, template: 'multiple_choice_image_right',
-				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.',
-				content: "multiplechoice image right",
+				image: '/img/image-placeholder_grey.svg',
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
 				options: [{text:'A'}, {text:'B'}, {text:'C'}, {text:'D'}, {text:'E'}]
 			},
 			{
 				id:8, template: 'text_image_left',
-				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.',
-				content: "text image left"
+				image: '/img/image-placeholder_grey.svg',
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. "
 			},
 			{
 				id:9, template: 'text_image_none',
-				content: "text image none"
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. "
 			},
 			{
 				id:10, template: 'text_image_right',
-				image: 'https://dummyimage.com/600x400/ffffff/00bcd4.jpg&text=img.',
-				content: "text image right"
+				image: '/img/image-placeholder_grey.svg',
+				content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. "
 			}
 			
 		]
